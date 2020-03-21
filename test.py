fffc733a90d50d841d3ff38f983af47ce696d8a3
cip <cip@provide.ro>
2020-03-21 22:05:05 +0200

    commit

--- test.py
+++ test.py
@@ -5,11 +5,17 @@ from resources.digi.digi import Digi
 digi = Digi()
 html = digi.login(sys.argv[1], sys.argv[2])
 
-cats = digi.scrapCats(html)
-print(cats)
-channels = digi.scrapChannels('/filme')
-print(channels)
-print(channels[0])
+#cats = digi.scrapCats(html)
+#print(cats)
+
+#cats = digi.scrapCatsPlay('/play')
+#print(cats)
+
+#channels = digi.scrapChannels('/play/filme')
+#channels = digi.scrapMovies('/play/filme')
+#print(channels)
+
+#print(channels[0])
 url = digi.scrapPlayUrl('/tematice/bbc-earth')
 #url = digi.scrapPlayUrl('/filme/hbo')
 #url = digi.scrapPlayUrl('/filme/cinemax-2')
